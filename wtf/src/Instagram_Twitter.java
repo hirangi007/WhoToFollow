0e81a72d2116513f0cc9ed1147b0c5ff9b1f1d52
hirangi <hirangi@hirangi-Lenovo-G500s>
2017-02-11 21:27:19 -0500

    people you may know for reference

--- wtf/src/Instagram_Twitter.java
+++ wtf/src/Instagram_Twitter.java
@@ -1,17 +1,12 @@
  import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
+
   import org.apache.hadoop.io.Text;
-  import org.apache.hadoop.mapreduce.Job;
-  import java.util.function.Predicate;
-  import org.apache.hadoop.conf.Configuration;
-  import org.apache.hadoop.fs.Path;
+ 
   import java.util.*;
   import org.apache.hadoop.mapreduce.Mapper;
   import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-  import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
  
 
   public class Instagram_Twitter {
