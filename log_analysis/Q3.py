c27d23df4eb360f81bad7db69699d07044f0e94d
hirangi <hirangi@hirangi-Lenovo-G500s>
2017-03-27 15:47:55 -0400

    q3 q4

---
+++ log_analysis/Q3.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 25, 2017
+
+@author: hirangi
+'''
+
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName("Spark Count")
+sc = SparkContext(conf=conf)
+sc.setLogLevel('ERROR')
+text_file1 = sc.textFile("/home/hirangi/Downloads/iliad/")
+text_file2 = sc.textFile("/home/hirangi/Downloads/odyssey/")
+
+filter_achille=text_file1.filter(lambda line: "Started Session " in line).map(lambda line: str(line.split()[-1])[:-1]).distinct()
+filter_achille1=text_file2.filter(lambda line: "Started Session " in line).map(lambda line: str(line.split()[-1])[:-1]).distinct()
+
+print filter_achille.collect()
+print filter_achille1.collect()
