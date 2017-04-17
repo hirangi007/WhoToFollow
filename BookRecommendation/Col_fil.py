914d255ec154fe881108bae3a0f17cdb3a263d6f
hirangi <hirangi@hirangi-Lenovo-G500s>
2017-04-17 16:04:39 -0400

    project

--- BookRecommendation/Col_fil.py
+++ BookRecommendation/Col_fil.py
@@ -4,10 +4,15 @@ Created on Mar 24, 2017
 @author: hirangi
 '''
 
+
+
+
 from pyspark.sql import Row, SparkSession
 from pyspark import SparkContext,SparkConf
-from pyspark.ml.recommendation import ALS
-from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
+
+
+
 
 
 def assignId(bookData):
@@ -19,7 +24,7 @@ def ratingsRows((isbn,value)):
     (ratingDetails,bookDetails) = value
     (userID,ratingValue) = ratingDetails
     (bookIds,bookTitle) = bookDetails
-    return Row(userId=int(userID), bookId=int(bookIds),rating=float(ratingValue), title=bookTitle)
+    return Rating(int(userID), int(bookIds),float(ratingValue)/2)
 
 conf = SparkConf().setMaster("local[*]").setAppName("BOOK_RECOMMENDATION")
 
@@ -29,28 +34,63 @@ spark = SparkSession \
     .appName("Collaborative Filtering") \
     .getOrCreate()
 
-data = sc.textFile("file:///home/hirangi/workspace/Test/ratings4.csv")
+
+
+data = sc.textFile("file:///home/hirangi/workspace/Test/ratings3.csv")
 books= sc.textFile("file:///home/hirangi/workspace/Test/books1.csv")
 i = 0
+
 booksRdd = books.map(lambda l: l.split(",")).map(assignId)
 parts = data.map(lambda row: row.split(",")).map(lambda l: (l[1],(l[0],l[2])))
 ratings = parts.join(booksRdd)
 ratingsData = ratings.map(ratingsRows)
-ratings = spark.createDataFrame(ratingsData)
-(training, test) = ratings.randomSplit([0.8, 0.2])
 
-# Build the recommendation model using ALS on the training data
-als = ALS(maxIter=5, regParam=0.01, userCol="userId", itemCol="bookId", ratingCol="rating")
-model = als.fit(training)
+# Build the recommendation model using Alternating Least Squares
+rank = 10
+numIterations = 10
+model = ALS.train(ratingsData, rank, numIterations)
 
-print model.itemFactors.collect()
+firstResult = model.recommendProducts(383,5)
+print " user: 383"
+print "======================"
+print " Top 5 Recommendation"
+print "======================"
+for temp in firstResult:
+    print temp.product
+print "======================"
 
-# Evaluate the model by computing the RMSE on the test data
-predictions = model.transform(test)
+firstResult = model.recommendProducts(276729,5)
+print " user: 276729"
+print "======================"
+print " Top 5 Recommendation"
+print "======================"
+for temp in firstResult:
+    print temp.product
+print "======================"
 
-evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
-                                predictionCol="prediction")
-rmse = evaluator.evaluate(predictions)
-print("Root-mean-square error = " + str(rmse))
+
+firstResult = model.recommendProducts(472,5)
+print " user: 472"
+print "======================"
+print " Top 5 Recommendation"
+print "======================"
+for temp in firstResult:
+    print temp.product
+print "======================"
 
 
+firstResult = model.recommendProducts(43397,5)
+print " user: 43397"
+print "======================"
+print " Top 5 Recommendation"
+print "======================"
+for temp in firstResult:
+    print temp.product
+print "======================"
+
+# Evaluate the model on training data
+testdata = ratingsData.map(lambda p: (p[0], p[1]))
+predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+ratesAndPreds = ratingsData.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
+print("Mean Squared Error = " + str(MSE))
